56039155b485cada207f762ed77fd291d7965c6c
Karina <milkokarina13@gmail.com>
2024-09-07 16:43:32 +0300

    Add contacts controller

---
+++ controllers/contactsController.js
@@ -0,0 +1,11 @@
+module.exports.createContact = (req, res) => {};
+
+module.exports.getContacts = (req, res) => {
+  res.status(200).send("OK!!!!!!");
+};
+
+module.exports.getContactById = (req, res) => {};
+
+module.exports.updateContactById = (req, res) => {};
+
+module.exports.deleteContactById = (req, res) => {};
