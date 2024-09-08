734dbe5d80b974d066398ff2e4b939b72a62b22c
Karina <milkokarina13@gmail.com>
2024-09-08 16:07:59 +0300

    Add validation contacts-update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { contactsController } = require("./controllers");
-const yup = require("yup");
-
+const { validate } = require("./middleware");
 const app = express();
 
 app.use(express.json());
@@ -59,39 +58,10 @@ app.get(
 // GET http://localhost:5000/contacts?page=1&results=3
 app.get("/contacts", contactsController.getContacts);
 
-const CREATE_CONTACT_VALIDATION_SCHEMA = yup.object({
-  name: yup
-    .string()
-    .trim()
-    .min(2)
-    .max(16)
-    .matches(/^[A-Z][a-z]{1,15}$/)
-    .required(),
-  telNumber: yup
-    .string()
-    .trim()
-    .length(13)
-    .matches(/^\+380\d{9}$/)
-    .required(),
-  birthday: yup.date().max(new Date()),
-});
-const validateContactOnCreate = async (req, res, next) => {
-  const { body } = req;
-  try {
-    const validatedContact = await CREATE_CONTACT_VALIDATION_SCHEMA.validate(
-      body
-    );
-    req.body = validatedContact;
-    next();
-  } catch (err) {
-    res.status(422).send(err.errors);
-  }
-};
-
 // POST http://localhost:5000/contacts
 app.post(
   "/contacts",
-  validateContactOnCreate,
+  validate.validateContactOnCreate,
   contactsController.createContact
 );
 
@@ -99,7 +69,11 @@ app.post(
 app.get("/contacts/:id", contactsController.getContactById);
 
 // PATCH http://localhost:5000/contacts/555654
-app.patch("/contacts/:id", contactsController.updateContactById);
+app.patch(
+  "/contacts/:id",
+  validate.validateContactOnUpdate,
+  contactsController.updateContactById
+);
 
 // DELETE http://localhost:5000/contacts/555654
 app.delete("/contacts/:id", contactsController.deleteContactById);
